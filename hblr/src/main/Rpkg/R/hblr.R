4fa85e1e9b6a33c4bfb5d8cac85cb9fef67d42a3
Dmitriy Lyubimov <dlyubimov@inadco.com>
2012-02-09 18:08:34 -0800

    WIP on hbl setup with R package.

--- hblr/src/main/Rpkg/R/hblr.R
+++ hblr/src/main/Rpkg/R/hblr.R
@@ -9,8 +9,10 @@ hbl$options = list();
 library("rJava");
 
 hbl.init = function () {
+	
     hbl$options$HBL_HOME <- Sys.getenv("HBL_HOME");
 	hbl$options$HADOOP_HOME <- Sys.getenv("HADOOP_HOME");
+	hbl$options$HBASE_HOME <- Sys.getenv("HBASE_HOME");
 	
 	if ( nchar(hbl$options$HADOOP_HOME) ==0 )
 		stop ("HADOOP_HOME not set");
@@ -18,5 +20,58 @@ hbl.init = function () {
 	if ( nchar(hbl$options$HBL_HOME) == 0 ) 
 		stop("HBL_HOME not set");
 	
+	if ( nchar(hbl$options$HBASE_HOME)==0 ) 
+		stop("HBASE_HOME not set");
+	
+	
+	cp1 <- list.files(
+			paste( hbl$options$HADOOP_HOME, "lib", sep="/" ), 
+			full.names = T,
+			pattern="\\.jar$")
+	
+	core <- list.files (
+			hbl$options$HADOOP_HOME,
+			full.names=T,
+			pattern=".*core.*\\.jar"
+			)
+	
+	cp2 <- list.files (
+			paste(hbl$options$HBL_HOME, "lib", sep="/"),
+			full.names = T,
+			pattern="\\.jar$"
+			)
+			
+	cp3 <- list.files (
+			paste(hbl$options$HBL_HOME,"hbl/target/", sep="/"),
+			full.names = T,
+			pattern="\\.jar$"
+			)
+			
+#	cp4 <- list.files( 
+#			paste(hbl$options$HBASE_HOME,"/lib",sep=""),
+#			full.names = T,
+#			pattern="\\.jar$")
+	hb_core <- list.files (
+			hbl$options$HBASE_HOME,
+			full.names=T,
+			pattern=".*hbase.*\\.jar")
+	
+	# TODO: pig classpath, too?
+	
+	hconf <- Sys.getenv("HADOOP_CONF")
+	
+	if (hconf == "")
+		hconf <- paste(hbl$options$HADOOP_HOME,"conf",sep="/")
+	
+	hbaseConf <- paste(hbl$options$HBASE_HOME,"conf",sep="/")
+	
+
+    hbl$classpath <- c(cp1,cp2,cp3,core,hb_core, hconf,hbaseConf)
+	
+	.jinit(classpath = hbl$classpath )	
+
+	hbl$conf <- new(J("org.apache.hadoop.conf.Configuration"));
+	hbl$conf <- J("org.apache.hadoop.hbase.HBaseConfiguration")$create(hbl$conf);
 	
 }
+
---
+++ hblr/src/main/Rpkg/tests/tests.R
@@ -0,0 +1,26 @@
+# TODO: Add comment
+# 
+# Author: dmitriy
+###############################################################################
+
+
+test.my <- function() { }
+
+
+setwd( getSrcDirectory(test.my))
+setwd("..")
+setwd("R")
+source("hblr.R")
+
+
+
+
+
+
+test1 <- function () {
+	hbl.init()
+}
+
+
+
+test1()
