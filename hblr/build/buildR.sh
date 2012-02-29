b060fa63eaf3e7444cfda0ad3ad362ac5aef4bf4
Dmitriy Lyubimov <dlyubimov@inadco.com>
2012-02-29 14:49:51 -0800

    setting up R profile for building R package part

---
+++ hblr/build/buildR.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# compiles R stuff. 
+# had problems with putting this into 
+# maven antrun.
+# param #1 -> R pkg version
+# param #1 -> build version 
+
+if [ $# -ne 2 ]; then 
+  echo 'usage: buildR.sh <r-package> <maven-version>'
+  exit 1
+fi
+
+pkgName=$1
+mver=$2
+d=`date +%F`
+
+# strip -SNAPSHOT things that create problems 
+rver=`sed 's/-SNAPSHOT\$/-00000000/' <<< $mver`
+
+echo $ver
+
+sed "s/^Version:/& ${rver}/" DESCRIPTION.tmpl | sed "s/^Date:/& ${d}/" | \
+sed "s/^Package:/& ${pkgName}/" > ../src/main/Rpkg/DESCRIPTION && \
+pushd ../target && \
+R CMD build ../src/main/Rpkg && \
+mv ${pkgName}_${rver}.tar.gz ${pkgName}-${mver}-rpkg.tar.gz && \
+popd
