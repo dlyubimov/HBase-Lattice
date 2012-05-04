8e34d1479ad390eb0d95bedd9eeb7c0d0ccc18a5
Dmitriy Lyubimov <dlyubimov@inadco.com>
2012-05-04 12:25:29 -0700

    install helper

--- hblr/install.sh
+++ hblr/install.sh
@@ -4,4 +4,4 @@
 
 MVN='mvn clean install -DskipTests -DR'
 
-sudo R CMD REMOVE hblr; { $MVN && sudo HADOOP_HOME=$HADOOP_HOME HBASE_HOME=$HBASE_HOME R_COMPILE_PKGS=1 R CMD INSTALL --build target/hblr-0.2.0-SNAPSHOT-rpkg; }
+sudo R CMD REMOVE hblr; { $MVN && sudo HADOOP_HOME=$HADOOP_HOME HBASE_HOME=$HBASE_HOME R_COMPILE_PKGS=1 R CMD INSTALL --build target/hblr-0.2.1-SNAPSHOT-rpkg; }
