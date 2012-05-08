cd54070c684c11d6751d507267c85a1ff3f9bf5e
Dmitriy Lyubimov <dlyubimov@inadco.com>
2012-05-08 12:43:21 -0700

    version correction in the snapshot installation helper

--- hblr/install.sh
+++ hblr/install.sh
@@ -3,5 +3,6 @@
 # for this to work. Also have maven executable around
 
 MVN='mvn clean install -DskipTests -DR'
+VER=0.2.3-SNAPSHOT
 
-sudo R CMD REMOVE hblr; { $MVN && sudo HADOOP_HOME=$HADOOP_HOME HBASE_HOME=$HBASE_HOME R_COMPILE_PKGS=1 R CMD INSTALL --build target/hblr-0.2.1-SNAPSHOT-rpkg; }
+sudo R CMD REMOVE hblr; { $MVN && sudo HADOOP_HOME=$HADOOP_HOME HBASE_HOME=$HBASE_HOME R_COMPILE_PKGS=1 R CMD INSTALL --build target/hblr-${VER}-rpkg; }
